0db8bfa5d1b9d0570f94cd9e3fc8cd5e20b94579
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-09-12 00:30:14 +0100

    Add MaximumSubarray problem

---
+++ leetCode/medium/MaximumSubarray.java
@@ -0,0 +1,22 @@
+package leetCode.medium;
+
+import java.lang.Math;
+
+public class MaximumSubarray {
+    // T - O(n)
+    // S - O(1)
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        MaximumSubarray m = new MaximumSubarray();
+        System.out.println(m.maxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+    }
+}
